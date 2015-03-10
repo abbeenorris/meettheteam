8d045b2bbeeee74f849557fc6f035c884618f881
Abigail Norris <abbeenorris@hotmail.co.uk>
2015-03-10 16:14:05 +0000

    Updated url for George's blog

--- about.php
+++ about.php
@@ -17,7 +17,7 @@
             <div class="box">
                 <p>This website has been created by George Allard, Abbie Norris and Ged Griska who are all Digital Media Design students. We've been given a brief of creating a social web based application that addresses a specific "problem" of a target audience of our choice. Through group meetings we have created a database which holds information for the website, and we have also identified a sector of society which we wish to target. This collaborative project will work alongside our own personal blogs to explain steps taken in this project (links below).</p>
                 
-                <p><a href="https://georgeallard.wordpress.com/">George Allard Blog</a></p>
+                <p><a href="http://georgeallarddmd.wordpress.com/">George Allard Blog</a></p>
                 <p><a href="http://dmdbournemouth.tumblr.com/">Abbie Norris Blog</a></p>
                 <p><a href="https://gedg.wordpress.com/">Ged Griska Blog</a></p>
                 
