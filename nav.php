01518a23210dbfba5c0f8e36b45d4588549a7945
Abigail Norris <abbeenorris@hotmail.co.uk>
2015-03-12 13:21:16 +0000

    Update nav.php
    added internal links to website - included form and day out pages

--- nav.php
+++ nav.php
@@ -4,6 +4,8 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="contact.php">Contact</a></li>
+        <li><a href="dayout.php">Meet The Team</a></li>
+        <li><a href="form.php">Submit your day out</a></li>
     </ul>
 
 </div>
