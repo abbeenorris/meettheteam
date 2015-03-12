09dced635df5e44fd52f6fb531548f2aa4724852
Abigail Norris <abbeenorris@hotmail.co.uk>
2015-03-12 13:35:22 +0000

    Update nav.php
    updated nav link

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="contact.php">Contact</a></li>
-        <li><a href="dayout.php">Meet The Team</a></li>
+        <li><a href="team.php">Meet The Team</a></li>
         <li><a href="form.php">Submit your day out</a></li>
     </ul>
 
