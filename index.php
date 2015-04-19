84e595b1494197a3504329c69fa7f8bd51f1d9ed
Abigail Norris <abbeenorris@hotmail.co.uk>
2015-04-19 22:24:00 +0100

    Update index.php
    changed index to be the login page

--- index.php
+++ index.php
@@ -1,27 +1,45 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
 
-<!doctype html>
+<?php if(isset($_SESSION["username"])) { ?>
+	<form action="submit-day.php" method="post">
+	    Name: <input type="text" name="name" value="" />
+	    Description: <input type="text" name="description" value="" />
+	    Location: <input type="text" name="location" value="" />
+	    Rating: <select name="rating">
+	                <option value="1">1</option>
+	                <option value="2">2</option>
+	                <option value="3">3</option>
+	                <option value="4">4</option>
+	                <option value="5">5</option>
+	            </select>
+	    <input type="submit" name="submit" value="Submit" />
+	</form>
+	<a href="logout.php">Logout</a>
+<?php } else { ?>
+	<div class="login-box">
+	    <form action="login.php" method="post">
+	        <p>Username:</p> <input type="text" name="username" value=""><br>
+	        <p>Password:</p> <input type="password" name="password" value=""><br>
+	        <input type="submit" name="login" value="Login">
+	    </form>
+	</div>
 
-<html>
-    <head>
-        <title>Format Code</title>
-        <link href="css/styles.css" rel="stylesheet" >
-    </head>
-    <body>
-        
-        <div class="container">
-        
-       <?php include 'nav.php'; ?> 
-        
-        <div class="top-title">
-                <h1>Day's Out!</h1>
-            </div>
-            
-                 <div class="top-title">
-                <p>Choose from a selection of Day's out below!</p>
-                
-            </div>
-            
 
-</div>
-</body>
-</html>
+
+	<div class="login-box">
+		<h1>Register</h1>
+	    <form action="register.php" method="post">
+	        <p>Username:</p> <input type="text" name="username" value=""><br>
+	        <p>Password:</p> <input type="password" name="password" value=""><br>
+	        <input type="submit" name="register" value="Register">
+	    </form>
+	</div>
+
+<?php } ?>
+
+
+
+<?php include_once("../includes/templates/footer.php"); ?>
