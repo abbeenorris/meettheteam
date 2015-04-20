2c4c792eb14cc8cc3639f30d9199173e5cc4b0c3
Abigail Norris <abbeenorris@hotmail.co.uk>
2015-04-20 10:28:51 +0100

    Update dayout.php
    Finally had the information sent over to put into days out page

--- dayout.php
+++ dayout.php
@@ -23,8 +23,184 @@
                 
             </div>
             
+            </div>
+            
+            <div class="top-title">
+            <p><h1><b>Active Day Out</b></h1> </p>
+            
+            </div>
+            
+        
+            
+            <div class="day-box">
+            <h1>Rockreff</h1>
+                <p><b>Description:</b> At the heart of the Pier you will find the brand new RockReef Centre featuring 4 great attractions in one, bringing the great outdoors indoors. There are 25 Clip N’ Climb challenges, the fun climbing concept that is taking the world by storm. The Pier Cave adventure, HighLine aerial obstacle course as well as a vertical drop slide, and the leap of faith jumping challenge. </p>
+                <p><b>Opening times:</b> The core opening hours 11:00 to 19:00, but can vary depending on the season. If you wanted to book a specific date and time, please check on our ticket sales website for availability.</p>
+                <p><b>Location:</b> Bournemouth Pier, Bournemouth BH2 5AA</p>
+                <p><b>Webiste:</b><a href="http://www.rockreef.co.uk/">http://www.rockreef.co.uk/</a></p>
+            
+            
+            </div>
+            
+                <div class="day-box">
+            <h1>Laser Quest Bournemouth</h1>
+                <p><b>Description:</b> Laser Quest is in essence a game of tag with a twist! Points are awarded every time you 'zap' another player, with points deducted every time you are 'zapped'. At the end of each game, scores are displayed on our plasma score screen, with each player receiving an individual score card that has a breakdown of the score. </p>
+                <p><b>Opening times:</b>Monday - Thursday	10am - 10pm
+Friday & Saturday	10am - 11pm
+Sunday	10am - 9pm
+</p>
+                <p><b>Location:</b>The Old Super Bowl, Geln Ferm Road, Bournemouth, Dorset, BH1 2LZ, United Kingdom
+ </p>
+                <p><b>Webiste:</b><a href="http://www.laserquestbournemouth.co.uk/">http://www.laserquestbournemouth.co.uk/</a></p>
+            
+            
+            </div>
+            
+             <div class="day-box">
+            <h1>Surfsteps</h1>
+                <p><b>Description:</b> Surf Steps, Bournemouth Surf School has been situated on Bournemouth beach for 8 years. 
+A Surfing GB Centre of Excellence, we cater for all abilities in Surfing and paddle boarding (SUP), including coaching courses, education and hire.
+ </p>
+                <p><b>Opening times:</b>Mon - Sun: 09:00–17:00
+</p>
+                <p><b>Location:</b>Toft Steps, Undercliff Drive, BH5 1BN Bournemouth </p>
+                <p><b>Webiste:</b><a href="http://www.bournemouthsurfschool.co.uk/">http://www.bournemouthsurfschool.co.uk/</a></p>
+            
+            
+            </div>
+            
+             <div class="top-title">
+            <p><h1><b>Social Day Out</b></h1> </p>
+            
+            </div>
+            
+              
+            
+             <div class="day-box">
+            <h1>Oceanarium</h1>
+                <p><b>Description:</b> Situated on Bournemouth seafront, the award-winning Oceanarium takes you on an underwater adventure through some of the world's most amazing waters. Home to hundreds of awesome creatures, come face to face with stunning sharks, curious green sea turtles, graceful stingrays, colourful clownfish and the very cute, Oriental small clawed otters. </p>
+                <p><b>Opening times:</b>Mon - Sun:	10:00–18:00
+</p>
+                <p><b>Location:</b>Pier Approach, West Beach, BH2 5AA Bournemouth </p>
+                <p><b>Webiste:</b><a href="http://www.oceanarium.co.uk/">http://www.oceanarium.co.uk/</a></p>
+            
+            
+            </div>
+        
+            
+             <div class="day-box">
+            <h1>Bowlplex</h1>
+                <p><b>Description:</b> At Bowlplex Tower Park, Poole there's something for everyone! State of the art bowling lanes are perfect for family activities. Chain tenpin bowling alley with an on-site snack bar, party deals, pool tables and arcade machines. </p>
+                <p><b>Opening times:</b>Mon-Wend 12:00 – 22:30 ; Thur 12:00 – 23:00 ; Fri 12:00 – 00:00 ; Sat 10:00 – 00:00 ; Sun 10:00- 22:30 </p>
+                <p><b>Location:</b> Tower Park Leisure Complex, Poole, BH12 4NY
+</p>
+                <p><b>Webiste:</b><a href="http://www.bowlplex.co.uk/locations/poole-tower-park/">http://www.bowlplex.co.uk/locations/poole-tower-park/</a></p>
+            
+            
+            </div>
+    
+      </div>
+            
+             <div class="day-box">
+            <h1>Splashdown</h1>
+                <p><b>Description:</b> Splashdown Waterpark is a great day out for family and friends. There’s something for everyone – from big kids to little ones – with some of the UK’s most exciting waterflumes. </p>
+                <p><b>Opening times:</b></b><a href="http://poole.splashdownwaterparks.co.uk/index.php/pages/opening_times.html">Please refer for opening times</a></p></p>
+                <p><b>Location:</b>Splashdown Poole, Tower Park, Poole, Dorset, BH12 4NY
+
+ </p>
+                <p><b>Webiste:</b><a href="http://poole.splashdownwaterparks.co.uk/"></a>http://poole.splashdownwaterparks.co.uk/</p>
+            
+            
+            </div>
+
+   <div class="top-title">
+            <p><h1><b>Shopping Day Out</b></h1> </p>
+            
+            </div>
+            
+
+            
+             <div class="day-box">
+            <h1>Castlepoint Shoppin Park</h1>
+                <p><b>Description:</b> Castlepoint Shopping Park is a unique retail experience. Located on the edge of Bournemouth (A3060), Castlepoint is the largest shopping park in the UK and has many of the biggest stores in the region. 
+
+In addition to B&Q and three large anchors, Sainsbury’s, Marks & Spencer and Asda, we are home to dozens of large High Street brands such as Next, Gap, H&M, New Look and much more, as well as restaurants and cafes.
+ </p>
+                <p><b>Opening times:</b>Mon – Fri 9:00 – 20:00 ; Sat 9:00 – 19:00 ; Sun 10:30 – 16:30 ; Band Holidays 10:00 – 17:00</p>
+                <p><b>Location:</b>Castle Lane West, Bournemouth, Dorset BH8 9UZ </p>
+                <p><b>Webiste:</b><a href="http://www.castlepointshopping.com/">http://www.castlepointshopping.com/</a></p>
+            
+            
+            </div>
+  
+            
+             <div class="day-box">
+            <h1>Bournemouth Town Centre </h1>
+                <p><b>Description:</b> Bournemouth town centre has a pedestrianised high street with a delightful mix of department stores, brand name stores, high street fashion stores and intriguing independent stores. </p>
+                <p><b>Opening times:</b>Differs . Approx 10:00 – 17:30 </p>
+                <p><b>Location:</b>Bournemouth Town Centre  </p>
+                <p><b>Webiste:</b><a href="http://bournemouth.co.uk/shopping/">http://bournemouth.co.uk/shopping/</a></p>
+            
+            
+            </div>
+
+<div class="day-box">
+            <h1>Buzzing Boscombe</h1>
+                <p><b>Description:</b> Boscombe is situated on the coast with it’s own pier, beautiful gardens and flourishing local tourism industry. The main shopping area is based around a pedestrian precinct, including part of Sea Road, with shops, cafes and restaurants surrounding this open area. </p>
+                <p><b>Opening times:</b>Differs Approx 10:00 – 17:30</p>
+                <p><b>Location:</b>Bournemouth, Boscombe  </p>
+                <p><b>Webiste:</b><a href="http://bournemouth.co.uk/shopping/">http://bournemouth.co.uk/shopping/</a></p>
+            
+            
+            </div>
+
+ <div class="top-title">
+     <p><h1><b>Cultural Day Out</b></h1> </p>
+            
+            </div>
+
+<div class="day-box">
+            <h1>Bournemouth Aviation Museum</h1>
+                <p><b>Description:</b> The Bournemouth Aviation Museum is an aviation museum located next to Bournemouth International Airport, near the village of Hurn in Christchurch. It houses a number of aircraft, aero engines, cockpits and a double-decker bus. </p>
+                <p><b>Opening times:</b>Oct 26th 2014 to March 28th 2015 open daily - 10am to 4pm
+
+March 29th 2015 to Oct 25th 2015 open daily - 10am to 5pm
+</p>
+                <p><b>Location:</b>Merritown Lane, Christchurch, Dorset BH23 6BA </p>
+                <p><b>Webiste:</b><a href="http://www.aviation-museum.co.uk/">http://www.aviation-museum.co.uk/</a></p>
+            
+            
+            </div>
+
+<div class="day-box">
+            <h1>The Russell-Cotes Museum</h1>
+                <p><b>Description:</b> The Russell-Cotes Museum is an art gallery and museum in Bournemouth, England. It is located on the top of the East Cliff, next to the Royal Bath Hotel. </p>
+                <p><b>Opening times:</b>OPEN: 10am - 5pm TUESDAY - SUNDAY & BANK HOLIDAY MONDAYS
+
+CLOSED: Mondays, Good Friday and Christmas Day.
+
+The Russell-Cotes may be closed for functions on occasion
+</p>
+                <p><b>Location:</b>The Russell-Cotes Art Gallery and Museum, Russell Cotes Road, Bournemouth BH1 3AA </p>
+                <p><b>Webiste:</b><a href="http://www.russell-cotes.bournemouth.gov.uk/HomePage.aspx">http://www.russell-cotes.bournemouth.gov.uk/HomePage.aspx</a></p>
+            
+            
+            </div>
+
+<div class="day-box">
+            <h1>Poole Museum </h1>
+                <p><b>Description:</b> Poole Museum is a local history museum situated on the Lower High Street in the Old Town area of Poole, Dorset, and is part of the Borough of Poole Museum Service. Entrance to Poole Museum is free. </p>
+                <p><b>Opening times:</b>Mon - Sat: 10am - 5pm
+Sun: 12pm noon - 5pm
+</p>
+                <p><b>Location:</b>4 High Street, Poole, Dorset BH15 1BW </p>
+                <p><b>Webiste:</b><a href="http://www.poolemuseum.co.uk/">http://www.poolemuseum.co.uk/</a></p>
+            
+            
+            </div>
 
 </div>
 </body>
 </html>
+
 
